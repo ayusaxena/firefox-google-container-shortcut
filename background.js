7319b27468f978e895abf50482cbfd25050a9102
ayusaxena <ayush.saxena@charter.com>
2019-06-22 20:09:24 -0400

    now with a button

--- background.js
+++ background.js
@@ -1,3 +1,8 @@
+// Param values from https://developer.mozilla.org/Add-ons/WebExtensions/API/contextualIdentities/create
+const GOOGLE_CONTAINER_NAME = "Google";
+const GOOGLE_CONTAINER_COLOR = "blue";
+const GOOGLE_CONTAINER_ICON = "circle";
+
 function onError(e) {
   console.error(e);
 }
@@ -6,50 +11,34 @@ function debug(message) {
   console.debug(["[Google Container Shortcut] ", message]);
 }
 
-function onContainerCommand(command) {
-  if (command == 'ecs-new-tab-current-container') {
-    openTabInCurrentContainer();
-    return;
-  }
-
-  const matches = command.match(/^ecs-new-tab-container-(\d)$/);
-  if (matches.length == 2) {
-    openContainerTab(parseInt(matches[1]) - 1);
-    return;
-  }
-}
-
 function getContexts() {
-  return browser.contextualIdentities.query({});
+  return browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME});
 }
 
-async function openTabInCurrentContainer() {
-  browser.tabs.query({currentWindow:true, active:true}).then(function(results) {
-    if (!results || results.length < 1) {
-      return; // do nothing
-    }
-    let currentTab = results[0];
-    browser.tabs.create({cookieStoreId: currentTab.cookieStoreId});
-  });
+async function setupOrFetchContainer () {
+  // Use existing Google container, or create one
+  const contexts = await browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME});
+  if (contexts.length > 0) {
+    googleCookieStoreId = contexts[0].cookieStoreId;
+  } else {
+    const context = await browser.contextualIdentities.create({
+      name: GOOGLE_CONTAINER_NAME,
+      color: GOOGLE_CONTAINER_COLOR,
+      icon: GOOGLE_CONTAINER_ICON
+    });
+    googleCookieStoreId = context.cookieStoreId;
+  }
+  return googleCookieStoreId
 }
 
-async function openContainerTab(contextNumber) {
-  let contexts;
-  try {
-    contexts = await getContexts();
-  } catch (e) {
-    console.error(e);
-    return;
-  }
-
-  if (contextNumber > contexts.length - 1) {
-    return; // no container defined for this shortcut, do nothing
-  }
+async function openGoogleContainerTab() {
+  googleContextId = await setupOrFetchContainer();
 
   return browser.tabs.create({
-    cookieStoreId: contexts[contextNumber].cookieStoreId
+    cookieStoreId: googleContextId
   })
 }
 
 // [COMMANDS] register commands
-browser.commands.onCommand.addListener(onContainerCommand);
+browser.commands.onCommand.addListener(openGoogleContainerTab);
+browser.browserAction.onClicked.addListener(openGoogleContainerTab)
