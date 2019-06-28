1208f2550b09f2f9fb32cacfb6f67238ceff02b9
Ayush Saxena <ayush.saxena@charter.com>
2019-06-28 16:58:25 -0400

    remove other from container

--- background.js
+++ background.js
@@ -31,14 +31,43 @@ async function setupOrFetchContainer () {
   return googleCookieStoreId
 }
 
-async function openGoogleContainerTab() {
+async function openGoogleContainerTab(currentTab) {
   googleContextId = await setupOrFetchContainer();
 
+  if (currentTab.cookieStoreId != "firefox-default") {
+    browser.tabs.remove(currentTab.id);
+    return browser.tabs.create({
+      url: currentTab.url
+    })
+  }
+  console.error('dodis', googleContextId)
   return browser.tabs.create({
-    cookieStoreId: googleContextId
+    cookieStoreId: googleContextId,
+    url: "https://www.google.com/"
   })
+}
+
+function tabUpdate(tabId, changeInfo, tabInfo) {
+  // var re = *://*.google.???/*;
+  var re = /http(s?):\/\/(www?).google.(com|ad|ae|com.af|com.ag|com.ai|al|am|co.ao|com.ar|as|at|com.au|az|ba|com.bd|be|bf|bg|com.bh|bi|bj|com.bn|com.bo|com.br|bs|bt|co.bw|by|com.bz|ca|cd|cf|cg|ch|ci|co.ck|cl|cm|cn|com.co|co.cr|com.cu|cv|com.cy|cz|de|dj|dk|dm|com.do|dz|com.ec|ee|com.eg|es|com.et|fi|com.fj|fm|fr|ga|ge|gg|com.gh|com.gi|gl|gm|gp|gr|com.gt|gy|com.hk|hn|hr|ht|hu|co.id|ie|co.il|im|co.in|iq|is|it|je|com.jm|jo|co.jp|co.ke|com.kh|ki|kg|co.kr|com.kw|kz|la|com.lb|li|lk|co.ls|lt|lu|lv|com.ly|co.ma|md|me|mg|mk|ml|com.mm|mn|ms|com.mt|mu|mv|mw|com.mx|com.my|co.mz|com.na|com.nf|com.ng|com.ni|ne|nl|no|com.np|nr|nu|co.nz|com.om|com.pa|com.pe|com.pg|com.ph|com.pk|pl|pn|com.pr|ps|pt|com.py|com.qa|ro|ru|rw|com.sa|com.sb|sc|se|com.sg|sh|si|sk|com.sl|sn|so|sm|sr|st|com.sv|td|tg|co.th|com.tj|tk|tl|tm|tn|to|com.tr|tt|com.tw|co.tz|com.ua|co.ug|co.uk|com.uy|co.uz|com.vc|co.ve|vg|co.vi|com.vn|vu|ws|rs|co.za|co.zm|co.zw|cat)\/*/;
+  // console.error(tabId);
+  // console.error(changeInfo);
+  // console.error(tabInfo);
+
+  var timeno = (new Date()).getTime() - 10;
+  if ('url' in changeInfo && re.test(changeInfo.url) == false) {
+    if (tabInfo.cookieStoreId == "firefox-container-6") {
+      console.error("not google!");
+      browser.tabs.remove(tabInfo.id);
+      // browser.browsingData.removeHistory({since:timeno});
+      return browser.tabs.create({
+        url: tabInfo.url
+      })
+    }
+  }
 }
 
 // [COMMANDS] register commands
 browser.commands.onCommand.addListener(openGoogleContainerTab);
-browser.browserAction.onClicked.addListener(openGoogleContainerTab)
+browser.browserAction.onClicked.addListener(openGoogleContainerTab);
+browser.tabs.onUpdated.addListener(tabUpdate);
